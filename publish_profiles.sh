585552f0c062e295e8776a17103170ad566a72ee
scarapella <sean@carapella.net>
2026-04-01 18:58:36 -0400

    fixing path

--- publish_profiles.sh
+++ publish_profiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 cd "$(dirname "$0")"
 
 SOURCE_DIR="./profiles2/"
@@ -8,12 +8,10 @@ if [ ! -z "$1" ]; then
     SOURCE_DIR="$1"
 fi
 PROJECT="paddle-map"
-DEST_BUCKET="gs://na-ne2-paddlemap-profiles"
-DEST_DIR="/profiles2"
-DEST_LOCATION="$DEST_BUCKET$DEST_DIR"
+DEST_BUCKET="gs://na-ne2-paddlemap-profiles/"
 
 echo "source: \"$SOURCE_DIR\""
-echo "dest  : \"$DEST_LOCATION\""
+echo "dest  : \"$DEST_BUCKET\""
 echo ""
 
 # Check if source directory exists
@@ -22,5 +20,5 @@ if [ ! -d "$SOURCE_DIR" ]; then
     exit 1
 fi
 
-gcloud storage cp -r $SOURCE_DIR $DEST_LOCATION/ --project="$PROJECT" 
-echo "Profiles published successfully to $DEST_LOCATION" 
+gcloud storage cp -r $SOURCE_DIR $DEST_BUCKET --project="$PROJECT" 
+echo "Profiles published successfully to $DEST_BUCKET" 
